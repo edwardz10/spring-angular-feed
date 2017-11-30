d65e21804ccaf58def21fe7377781b50dd333dbd
eziyangulov <eduard.ziyangulov@netcracker.com>
2017-11-30 19:07:17 +0300

    commit

--- src/main/resources/static/js/app/controller.js
+++ src/main/resources/static/js/app/controller.js
@@ -2,10 +2,6 @@ var controllerModule = angular.module('usersAppControllers', []);
 
 controllerModule.controller('UserController', function($scope, $http, UserService) {
 
-    UserService.getUsers().then(function(users){
-        $scope.users = users;
-    });
-
     $scope.init = function () {
         console.log('AAA');
     	$scope.buttonLabel = "Start feed";
@@ -15,9 +11,16 @@ controllerModule.controller('UserController', function($scope, $http, UserServic
     $scope.controlFeed = function() {
         if ($scope.buttonLabel === 'Start feed') {
     	    console.log('Start feed');
+            UserService.startFeed();
             $scope.buttonLabel = 'Stop feed';
+                
+            UserService.getUsers().then(function(users){
+                $scope.users = users;
+            });
+
         } else if ($scope.buttonLabel === 'Stop feed'){
     	    console.log('Stop feed');
+            UserService.stopFeed();
             $scope.buttonLabel = 'Start feed';
         } else {
             console.log('Not implemented');
--- src/main/resources/static/js/app/service.js
+++ src/main/resources/static/js/app/service.js
@@ -9,4 +9,22 @@ servicesModule.service('UserService', function($http) {
         });
     }
 
+    this.startFeed = function() {
+        console.log("service start feed....");
+        $http.post('http://localhost:8080/spring-angular-feed-0.0.1-SNAPSHOT/api/users/start', '').then(function() {
+            console.log('service start feed success');
+        }).catch(function(err) {
+            console.log('service start feed failure');
+        });
+    }
+
+    this.stopFeed = function() {
+        console.log("service stop feed....");
+        $http.post('http://localhost:8080/spring-angular-feed-0.0.1-SNAPSHOT/api/users/stop', '').then(function() {
+            console.log('service stop feed success');
+        }).catch(function(err) {
+            console.log('service stop feed failure');
+        });
+    
+    }
 });
