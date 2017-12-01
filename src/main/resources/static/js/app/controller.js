06b7fd0e76568e01171f6c8eee4e84d9128d6544
eziyangulov <eduard.ziyangulov@netcracker.com>
2017-12-01 18:31:15 +0300

    commit

--- src/main/resources/static/js/app/controller.js
+++ src/main/resources/static/js/app/controller.js
@@ -1,30 +1,34 @@
 var controllerModule = angular.module('usersAppControllers', []);
 
-controllerModule.controller('UserController', function($scope, $http, UserService) {
+controllerModule.controller('UserController', function($scope, $http, $interval, UserService) {
 
     $scope.init = function () {
         console.log('AAA');
-    	$scope.buttonLabel = "Start feed";
+        $scope.buttonLabel = "Start feed";
         $scope.users = [];
-    }    
-    
+    }
+
+    feedUsersAtInterval = function() {
+        UserService.getUsers().then(function(users){
+            $scope.users = users;
+        });
+    }
+
     $scope.controlFeed = function() {
         if ($scope.buttonLabel === 'Start feed') {
-    	    console.log('Start feed');
+            console.log('Start feed');
             UserService.startFeed();
             $scope.buttonLabel = 'Stop feed';
-                
-            UserService.getUsers().then(function(users){
-                $scope.users = users;
-            });
+
+            $interval(callAtInterval, 2000);
 
         } else if ($scope.buttonLabel === 'Stop feed'){
-    	    console.log('Stop feed');
+            console.log('Stop feed');
             UserService.stopFeed();
             $scope.buttonLabel = 'Start feed';
         } else {
             console.log('Not implemented');
         }
     }
-    
+
 });
